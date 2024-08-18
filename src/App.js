70d0a7731b20ac86ca979115a0a984f7e08de8a5
SYL4Git <seungyeollee4@gmail.com>
2024-08-18 18:39:05 +0900

    240818 1838 react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import './App.css';
+// react route = https://www.youtube.com/watch?v=SMq1IQRweDc
+import { BrowserRouter, Router, Routes, Route } from 'react-router-dom';
 
 import Header from './component/Header';
+import Weather from './component/Weather';
 import TodoList from './pages/TodoList';
+import Diary from './pages/Diary';
+import Notes from './pages/Notes';
+
+import './App.css';
 
 function App() {
 	return (
-		<div className='App'>
-			<Header />
-			<TodoList />
-		</div>
+		<BrowserRouter>
+			<Router>
+				<Header />
+				<Weather />
+				<Routes>
+					<Route path='/todolist' element={<TodoList />} />
+					<Route path='/diary' element={<Diary />} />
+					<Route path='/notes' element={<Notes />} />
+				</Routes>
+			</Router>
+		</BrowserRouter>
 	);
 }
 

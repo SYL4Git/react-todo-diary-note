6f0c92d9a777759252b5d831fa60f3a4b55113be
SYL4Git <seungyeollee4@gmail.com>
2026-07-08 01:09:36 +0900

    Initial commit

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,5 +1,9 @@
 import { useCallback, useEffect, useRef, useReducer, useMemo } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const Diary = () => {};
+const Diary = () => {
+	<section>
+		<div className='pgDescription'>간단한 일기를 쓸 수 있는 공간입니다.</div>
+	</section>;
+};
 export default Diary;

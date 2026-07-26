a73d910c69d7cf49c30d34845836836f96a5b238
SYL4Git <seungyeollee4@gmail.com>
2026-07-26 20:32:11 +0900

    260726 20:32

--- src/pages/DiaryContent.js
+++ src/pages/DiaryContent.js
@@ -1,1 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const DiaryContent = () => {
+	const [diaryList, setDiaryList] = useState([]);
+
+	useEffect(() => {
+		const saved = localStorage.getItem('diaryList');
+		if (saved) {
+			setDiaryList(JSON.parse(saved));
+		}
+	}, []);
+	return (
+		<section className='diaryContent'>
+			<div className='diaryContainer'>
+				<dl>
+					<dt className='diaryDateContainer'>날짜: </dt>
+					<dd className='diaryDate'></dd>
+					<dt className='diaryTitleContainer'>제목: </dt>
+					<dd className='diaryTitle'></dd>
+				</dl>
+			</div>
+		</section>
+	);
+};
+
+export default DiaryContent;

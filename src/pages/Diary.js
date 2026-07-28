b80ad38ef182853a8672f1c7d384f7a6d784d35d
SYL4Git <seungyeollee4@gmail.com>
2026-07-29 01:58:17 +0900

    260728

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useRef, useReducer, useMemo } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DiaryContent from './DiaryContent.js';
 
 const Diary = () => {
 	return (
@@ -7,6 +8,7 @@ const Diary = () => {
 			<div className='pgDescription'>
 				간단한 일기를 쓸 수 있는 공간입니다.
 			</div>
+			<DiaryContent />
 		</section>
 	);
 };
--- src/pages/DiaryContent.js
+++ src/pages/DiaryContent.js
@@ -2,13 +2,16 @@ import { useState, useEffect } from 'react';
 
 const DiaryContent = () => {
 	const [diaryList, setDiaryList] = useState([]);
+	console.log('1 컴포넌트 함수 실행중');
 
 	useEffect(() => {
+		console.log('3 useeffect 실행됨');
 		const saved = localStorage.getItem('diaryList');
 		if (saved) {
 			setDiaryList(JSON.parse(saved));
 		}
 	}, []);
+	console.log('2 return 직전');
 	return (
 		<section className='diaryContent'>
 			<div className='diaryContainer'>
